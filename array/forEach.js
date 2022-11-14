24428e7893e7678d3da03ac67ef5b04cfa5c69b8
Serhii Slesarev <slesarev071290@gmail.com>
2022-11-14 16:56:01 +0200

    feat: add comments to forEach method

---
+++ array/forEach.js
@@ -0,0 +1,10 @@
+// Array.prototype.forEach()
+// Метод forEach() виконує надану функцію один
+// раз для кожного елементу масиву
+
+const array = ['a', 'b', 'c'];
+array.forEach((element) => console.log(element));
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
