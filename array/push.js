ec4564e414f9adebe509480ecc7967ee39d172dc
Serhii Slesarev <slesarev071290@gmail.com>
2022-11-14 14:34:09 +0200

    feat: add push method

---
+++ array/push.js
@@ -0,0 +1,11 @@
+// Array.prototype.push()
+
+const animals = ["pigs", "goats", "sheep"];
+
+const count = animals.push("cows");
+
+console.log(count);
+// expected output: 4
+
+console.log(animals);
+// expected output: Array ["pigs", "goats", "sheep", "cows"]
