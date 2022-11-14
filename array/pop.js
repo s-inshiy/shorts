58d6ca61774f46a97377c860cc8072460a0a50a5
Serhii Slesarev <slesarev071290@gmail.com>
2022-11-14 15:11:24 +0200

    feat: add pop method

---
+++ array/pop.js
@@ -0,0 +1,14 @@
+// Array.prototype.pop()
+
+const colors = [
+  'yellow',
+  'blue',
+  'green',
+  'pink',
+];
+
+console.log(colors.pop());
+// expected output: pink
+
+console.log(colors);
+// expected output: Array [ 'yellow', 'blue', 'green' ]
