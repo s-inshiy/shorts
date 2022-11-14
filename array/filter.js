ec2ca8e5dadfb8e90b63d0ebe63e513b3d533b04
Serhii Slesarev <slesarev071290@gmail.com>
2022-11-14 10:58:46 +0200

    feat: add filter method

--- array/filter.js
+++ array/filter.js
@@ -1,6 +1,9 @@
+// Array.prototype.filter();
+
 const numbers = [7, 87, 62, 21, 63, 4, 57, 100];
 
 const result = numbers.filter((number) => number < 50);
 
 console.log(result);
+
 // expected output: Array [ 7, 21, 4 ]
