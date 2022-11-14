401b27962f6cc2d9785938b7fd39a031dab4f42e
Serhii Slesarev <slesarev071290@gmail.com>
2022-11-14 16:54:22 +0200

    feat: add comments to map method

--- array/map.js
+++ array/map.js
@@ -1,8 +1,13 @@
 // Array.prototype.map();
+// Метод map() створює новий масив, який заповнюється
+// результатами виклику наданої функції
+// для кожного елемента масиву
 
 const numbers = [1, 4, 9];
 
-const result = numbers.map((number) => number * 2);
+const result = numbers.map(
+  (number) => number * 2
+);
 
 console.log(result);
 
