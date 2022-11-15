b98cc093bc0e9c4f2fae9aefe9ecf32fcf120394
Serhii Slesarev <slesarev071290@gmail.com>
2022-11-15 14:37:26 +0200

    feat: update forEach method

--- array/forEach.js
+++ array/forEach.js
@@ -1,10 +1,18 @@
 // Array.prototype.forEach()
+
+const items = [
+  { name: 'Book', price: 20 },
+  { name: 'Computer', price: 310 },
+  { name: 'Phone', price: 270 },
+  { name: 'TV', price: 180 },
+  { name: 'Bike', price: 490 },
+];
+
+function logItemPrice(item) {
+  console.log(item.price);
+}
+
+items.forEach(logItemPrice);
+
 // Метод forEach() виконує надану функцію один
 // раз для кожного елементу масиву
-
-const array = ['a', 'b', 'c'];
-array.forEach((element) => console.log(element));
-
-// expected output: "a"
-// expected output: "b"
-// expected output: "c"
