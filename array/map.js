a67375e6ccb07375db0fa01e5b0a023b7c4dbf58
Serhii Slesarev <slesarev071290@gmail.com>
2022-11-15 14:08:03 +0200

    feat: update map method

--- array/map.js
+++ array/map.js
@@ -1,14 +1,20 @@
 // Array.prototype.map();
+
 // Метод map() створює новий масив, який заповнюється
 // результатами виклику наданої функції
 // для кожного елемента масиву
 
-const numbers = [1, 4, 9];
+const items = [
+  { name: 'Book', price: 20 },
+  { name: 'Computer', price: 310 },
+  { name: 'Phone', price: 270 },
+  { name: 'TV', price: 180 },
+  { name: 'Bike', price: 490 },
+];
 
-const result = numbers.map(
-  (number) => number * 2
-);
+function getItemName(item) {
+  return item.name;
+}
+const itemNames = items.map(getItemName);
 
-console.log(result);
-
-// expected output: [2, 8, 18]
+console.log(itemNames);
